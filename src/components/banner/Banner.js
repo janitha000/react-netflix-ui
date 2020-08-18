04b09bbdbb9cb4a38ba364fdf59b03fc6d5bf959
Janitha Tennakoon <janitha000@gmail.com>
2020-08-18 12:12:38 +0800

    Added banner component

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,14 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import requests from '../../request'
+import axios from '../../axios'
 import './Banner.css'
 
-const Banner = () => {
-    return (
-        <div className='banner'>
-            <img className='banner__img'
-                src="https://images.indianexpress.com/2017/06/dunkirk-1200.jpg"
-                alt='banner' />
+const BASE_URL = 'https://image.tmdb.org/t/p/original/';
 
-        </div>
+const Banner = () => {
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            const movies = request.data.results;
+
+            const random = Math.floor(Math.random() * movies.length - 1)
+            setMovie(movies[random])
+        }
+
+        fetchData()
+    }, [])
+
+    const truncate = (str, n) => {
+        return str?.length > n ? str.substring(0, n - 1) + '...' : str
+    }
+
+    return (
+        <header className='banner'
+            style={{
+                backgroundSize: "cover",
+                backgroundImage: `url(${BASE_URL}/${movie?.backdrop_path})`,
+                backgroundPosition: 'center center'
+            }}
+        >
+            <div className="banner__content">
+                <h1 className="banner__title">{movie?.title || movie?.name || movie?.original_name}</h1>
+                <div className='banner_buttons'>
+                    <button className='banner__button'>Play</button>
+                </div>
+                <h1 className='banner__description'>{truncate(movie?.overview, 250)}</h1>
+            </div>
+            <div className="banner__fadebottom" />
+        </header>
+
     )
 }
 
