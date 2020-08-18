dcb6043caead254d01783f6d8ea29a60705c9de1
Janitha Tennakoon <janitha000@gmail.com>
2020-08-18 14:19:28 +0800

    Added google fonts

--- src/components/rowDetail/RowDetail.js
+++ src/components/rowDetail/RowDetail.js
@@ -2,14 +2,32 @@ import React from 'react'
 import './rowDetail.css'
 const BASE_URL = 'https://image.tmdb.org/t/p/original/';
 
+const truncate = (str, n) => {
+    return str?.length > n ? str.substring(0, n - 1) + '...' : str
+}
 
 const RowDetail = ({ movie }) => {
     return (
         <div className='rowdetail'>
-            <img
+            <header className='rowdetail__poster'
+                style={{
+                    backgroundSize: "cover",
+                    backgroundImage: `url(${BASE_URL}/${movie?.backdrop_path})`,
+                    backgroundPosition: 'center center'
+                }}
+            >
+                <div className='rowdetail__content'>
+                    <h1 className="rowdetail__title">{movie?.title || movie?.name || movie?.original_name}</h1>
+                    <div className='rowdetail_buttons'>
+                        <button className='rowdetail__button'>Play Trailer</button>
+                    </div>
+                    <h1 className='rowdetail__description'>{truncate(movie?.overview, 250)}</h1>
+                </div>
+            </header>
+            {/* <img
                 src={`${BASE_URL}${movie.backdrop_path}`}
                 alt={movie.name}
-                className='rowdetail__img' />
+                className='rowdetail__img' /> */}
         </div>
     )
 }
