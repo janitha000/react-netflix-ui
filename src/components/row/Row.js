419c57d54db5b9024ed31eeb764a17bd67207890
Janitha Tennakoon <janitha000@gmail.com>
2020-08-18 15:12:28 +0800

    Added trailers

--- src/components/row/Row.js
+++ src/components/row/Row.js
@@ -2,11 +2,25 @@ import React, { useState, useEffect } from 'react'
 import axios from '../../axios'
 import './Row.css'
 import RowDetail from '../rowDetail/RowDetail'
+import Youtube from 'react-youtube'
+import movieTrailer from 'movie-trailer'
+
 const BASE_URL = 'https://image.tmdb.org/t/p/original/';
 
 const Row = ({ title, fetchUrl }) => {
     const [movies, setMovies] = useState([]);
     const [selectedMovie, setSelectedMovie] = useState({})
+    const [trailerUrl, setTrailerUrl] = useState('')
+
+    const options = {
+        height: '390',
+        width: '640',
+        playerVars: {
+            // https://developers.google.com/youtube/player_parameters
+            autoplay: 1,
+        },
+    }
+
 
     useEffect(() => {
         async function fetchData() {
@@ -18,20 +32,38 @@ const Row = ({ title, fetchUrl }) => {
 
     }, [fetchUrl])
 
+    const showTrailer = (movie) => {
+        console.log(`calling youtube for ${movie?.name}`)
+        movieTrailer(movie?.title || movie?.name || movie?.original_name || "").then(url => {
+            const urlParams = new URLSearchParams(new URL(url).search);
+            setTrailerUrl(urlParams.get('v'))
+        }).catch(err => {
+            console.log(err);
+        })
+    }
+
+    const setNewMovieUrl = (movie) => {
+        setTrailerUrl('')
+        setSelectedMovie(movie)
+    }
+
     return (
         <div className="row">
-            <h2>{title}</h2>
+            <h2 className="row__title">{title}</h2>
             <div className='row__posters'>
                 {movies.map(movie => (
                     <img
                         key={movie.id}
                         className='row__poster'
-                        onClick={e => (selectedMovie.id === movie.id) ? setSelectedMovie({}) : setSelectedMovie(movie)}
+                        onClick={e => (selectedMovie.id === movie.id)
+                            ? setSelectedMovie({})
+                            : (trailerUrl === '') ? setSelectedMovie(movie) : setNewMovieUrl(movie)}
                         src={`${BASE_URL}${movie.poster_path}`} alt={movie.name} />
                 ))}
             </div>
             <div className='row__detail'>
-                {selectedMovie.id && <RowDetail movie={selectedMovie} />}
+                {selectedMovie.id && trailerUrl === '' && <RowDetail movie={selectedMovie} onTrailerClick={showTrailer} />}
+                {trailerUrl && <div className="row__youtube"> <Youtube videoId={trailerUrl} opts={options} /></div>}
             </div>
         </div>
     )
--- src/components/rowDetail/RowDetail.js
+++ src/components/rowDetail/RowDetail.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './rowDetail.css'
+
 const BASE_URL = 'https://image.tmdb.org/t/p/original/';
 
 const truncate = (str, n) => {
     return str?.length > n ? str.substring(0, n - 1) + '...' : str
 }
 
-const RowDetail = ({ movie }) => {
+const RowDetail = ({ movie, onTrailerClick }) => {
+
+
+
+
     return (
         <div className='rowdetail'>
             <header className='rowdetail__poster'
@@ -19,15 +24,12 @@ const RowDetail = ({ movie }) => {
                 <div className='rowdetail__content'>
                     <h1 className="rowdetail__title">{movie?.title || movie?.name || movie?.original_name}</h1>
                     <div className='rowdetail_buttons'>
-                        <button className='rowdetail__button'>Play Trailer</button>
+                        <button className='rowdetail__button' onClick={e => onTrailerClick(movie)}>Play Trailer</button>
                     </div>
                     <h1 className='rowdetail__description'>{truncate(movie?.overview, 250)}</h1>
                 </div>
             </header>
-            {/* <img
-                src={`${BASE_URL}${movie.backdrop_path}`}
-                alt={movie.name}
-                className='rowdetail__img' /> */}
+
         </div>
     )
 }
